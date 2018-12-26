9fa7e7ebe70cc702ad1031a16cb5bf0401938589
rundeploy <cristianmitul@gmail.com>
2018-12-26 17:55:07 +0000

    Stopped on section 6, lesson 60. TODO: review some errors!

---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UdemyExercice6
+{
+    public partial class frmAuthors : Form
+    {
+        public frmAuthors()
+        {
+            InitializeComponent();
+        }
+
+        OleDbConnection booksConn;
+        OleDbCommand authorsComm;
+        OleDbDataAdapter authorsAdapter;
+        DataTable authorsTable;
+        CurrencyManager authorsManager;
+
+
+        private void frmAuthors_Load(object sender, EventArgs e)
+        {
+            var connString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\PROJETOS\UdemyCourse (Alex)\UdemyProject 6\UdemyExercice6\Books.accdb; 
+                                Persist Security Info = False; ";
+            booksConn = new OleDbConnection(connString);
+            booksConn.Open();
+            authorsComm = new OleDbCommand("SELECT * from Authors order By Author", booksConn);
+            authorsAdapter = new OleDbDataAdapter();
+            authorsTable = new DataTable();
+            authorsAdapter.SelectCommand = authorsComm;
+            authorsAdapter.Fill(authorsTable);
+
+            txtAuthorID.DataBindings.Add("Text", authorsTable, "AU_ID");
+            txtAuthorName.DataBindings.Add("Text", authorsTable, "Author");
+            txtAuthorBorn.DataBindings.Add("Text", authorsTable, "Year_Born");
+            authorsManager = (CurrencyManager) BindingContext[authorsTable];
+
+            setAppState("View");
+  
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            authorsManager.Position--;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            authorsManager.Position++;
+        }
+
+        private void frmClosing(object sender, FormClosingEventArgs e)
+        {
+            booksConn.Close();
+            booksConn.Dispose();
+            authorsComm.Dispose();
+            authorsAdapter.Dispose();
+            authorsTable.Dispose();
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Record saved", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            setAppState("View");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult response;
+            response = MessageBox.Show("Ayre you sure you want do delete this record", "Delete",
+            MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+
+            if ( response == DialogResult.No)
+            {
+                return;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void setAppState(string appState)
+        {
+            switch (appState)
+            {
+                case "View":
+                    
+                    txtAuthorName.ReadOnly = true;
+                    txtAuthorBorn.ReadOnly = true;
+                    btnPrevious.Enabled = true;
+                    btnNext.Enabled = true;
+                    btnSave.Enabled = false;
+                    btnCancel.Enabled = false;
+                    btnAddNew.Enabled = true;
+                    btnDelete.Enabled = true;
+                    btnDone.Enabled = true;
+                    txtAuthorName.TabStop = false;
+                    txtAuthorBorn.TabStop = false;
+
+                    break;
+                default: //add and edit states
+                    
+                    txtAuthorName.ReadOnly = false;
+                    txtAuthorBorn.ReadOnly = false;
+                    btnPrevious.Enabled = false;
+                    btnNext.Enabled = false;
+                    btnSave.Enabled = true;
+                    btnCancel.Enabled = true;
+                    btnAddNew.Enabled = false;
+                    btnDelete.Enabled = false;
+                    btnDone.Enabled = false;
+                    txtAuthorName.Focus();
+                    break;
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            setAppState("Edit");
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            setAppState("Add");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            setAppState("View");
+        }
+
+        private void txtAuthorBorn_Keypress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0' && e.KeyChar <= '9') || e.KeyChar == 8)
+            {
+                e.Handled = false;
+                lblWrongInput.Visible = false;
+            }
+            else
+            {
+                e.Handled = true;
+                lblWrongInput.Visible = true;
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            string message = "";
+            int inputYear, currentYear;
+            bool allOK = true;
+            if (txtAuthorName.Text.Trim().Equals(""))
+            {
+                message = "Author's name is required" + "r\n";
+                txtAuthorName.Focus();
+                allOK = false;
+            }
+            if (!txtAuthorBorn.Text.Trim().Equals(""))
+            {
+                inputYear = Convert.ToInt32(txtAuthorBorn.Text);
+                currentYear = DateTime.Now.Year;
+                if (inputYear >= currentYear)
+                {
+                    message += "Invalid Year";
+                    txtAuthorBorn.Focus();
+                    allOK = false;
+                }
+            }
+
+            if (!allOK)
+            {
+                MessageBox.Show(message, "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            return allOK;
+        }
+
+        private void txtAuthorName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                txtAuthorBorn.Focus();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UdemyExercice6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UdemyExercice6")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("af474d9b-36a7-4f9d-812e-e70cfe2681fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
